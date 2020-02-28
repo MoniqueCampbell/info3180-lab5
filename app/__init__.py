52f8063195a16376717625939326e6caad7c4b63
MoniqueCampbell <43890502+MoniqueCampbell@users.noreply.github.com>
2020-02-27 20:34:50 -0500

    Add files via upload
    lab5

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,9 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
+app.config['SECRET_KEY'] = "cats"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:Monique5!@localhost/lab5"
+#postgresql://user:password@localhost/database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
